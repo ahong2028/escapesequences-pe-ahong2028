1f2a36a714c6160a6a5029839bbe0b82eb884dba
ahong2028 <ahong2028@pingry.org>
2025-01-27 16:43:21 -0500

    Create Escape.py

---
+++ Escape.py
@@ -0,0 +1,16 @@
+# Albert Hong
+# Intro to Programming - Block 4
+# Escape Sequences
+
+# Main function
+def main():
+	# Printing the statment: I can split lines by using the \ character
+	print("I can split lines by using the \\ character.")
+	# Printing the statement: I can print new lines with the \n sequence
+	print("I can print new lines with the \\n sequence.")
+	# Printing the statement: I can print tabs with the \t sequence
+	print("I can print tabs with the \\t sequence.")
+	print()
+
+# Calling main
+main()
